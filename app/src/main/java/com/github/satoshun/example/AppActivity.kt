a59e79de6e73c76e7ccac31079717a855fbc2079
SatoShun <shun.sato1@gmail.com>
2019-10-21 02:24:07 +0000

    appFragment

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -1,19 +1,15 @@
 package com.github.satoshun.example
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.github.satoshun.example.databinding.AppActBinding
-import kotlinx.coroutines.launch
-import okhttp3.Cache
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.create
-import retrofit2.http.GET
+import com.github.satoshun.example.databinding.AppFragBinding
 
 class AppActivity : AppCompatActivity() {
   private lateinit var binding: AppActBinding
@@ -21,73 +17,24 @@ class AppActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.app_act)
-
-    run {
-      val okhttp = OkHttpClient.Builder()
-        .addNetworkInterceptor(object : Interceptor {
-          override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-            val request = chain.request()
-            return chain.proceed(
-              request
-                .newBuilder()
-                .addHeader("Cache-Control", "max-stale=60")
-                .build()
-            )
-          }
-        })
-        .cache(Cache(cacheDir, (10 * 1024 * 1024).toLong()))
-        .build()
-
-      val retrofit: Api = Retrofit.Builder()
-        .baseUrl("https://api.github.com/")
-        .client(okhttp)
-        .build()
-        .create()
-
-      binding.maxStale.setOnClickListener {
-        lifecycleScope.launch {
-          val r = retrofit.get()
-          Log.d("priorResponse", r.raw().priorResponse.toString())
-          Log.d("cacheResponse", r.raw().cacheResponse.toString())
-          Log.d("networkResponse", r.raw().networkResponse.toString())
-        }
-      }
-    }
-
-    run {
-      val okhttp = OkHttpClient.Builder()
-        .addNetworkInterceptor(object : Interceptor {
-          override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-            val request = chain.request()
-            return chain.proceed(
-              request
-                .newBuilder()
-                .build()
-            )
-          }
-        })
-        .cache(Cache(cacheDir, (10 * 1024 * 1024).toLong()))
-        .build()
-
-      val retrofit: Api = Retrofit.Builder()
-        .baseUrl("https://api.github.com/")
-        .client(okhttp)
-        .build()
-        .create()
-
-      binding.cache.setOnClickListener {
-        lifecycleScope.launch {
-          val r = retrofit.get()
-          Log.d("priorResponse", r.raw().priorResponse.toString())
-          Log.d("cacheResponse", r.raw().cacheResponse.toString())
-          Log.d("networkResponse", r.raw().networkResponse.toString())
-        }
-      }
-    }
   }
 }
 
-interface Api {
-  @GET("users/satoshun")
-  suspend fun get(): Response<Unit>
+class AppFragment : Fragment() {
+  private lateinit var binding: AppFragBinding
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    binding = AppFragBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.basicCache.setOnClickListener {
+      findNavController().navigate(
+        AppFragmentDirections.navAppToBasicCache()
+      )
+    }
+  }
 }
---
+++ app/src/main/java/com/github/satoshun/example/basiccache/BasicCacheFragment.kt
@@ -0,0 +1,97 @@
+package com.github.satoshun.example.basiccache
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.github.satoshun.example.databinding.BasicCacheFragBinding
+import kotlinx.coroutines.launch
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.create
+import retrofit2.http.GET
+
+class BasicCacheFragment : Fragment() {
+  private lateinit var binding: BasicCacheFragBinding
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    binding = BasicCacheFragBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    run {
+      val okhttp = OkHttpClient.Builder()
+        .addNetworkInterceptor(object : Interceptor {
+          override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            val request = chain.request()
+            return chain.proceed(
+              request
+                .newBuilder()
+                .addHeader("Cache-Control", "max-stale=60")
+                .build()
+            )
+          }
+        })
+        .cache(Cache(requireActivity().cacheDir, (10 * 1024 * 1024).toLong()))
+        .build()
+
+      val retrofit: Api = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .client(okhttp)
+        .build()
+        .create()
+
+      binding.maxStale.setOnClickListener {
+        lifecycleScope.launch {
+          val r = retrofit.get()
+          Log.d("priorResponse", r.raw().priorResponse.toString())
+          Log.d("cacheResponse", r.raw().cacheResponse.toString())
+          Log.d("networkResponse", r.raw().networkResponse.toString())
+        }
+      }
+    }
+
+    run {
+      val okhttp = OkHttpClient.Builder()
+        .addNetworkInterceptor(object : Interceptor {
+          override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            val request = chain.request()
+            return chain.proceed(
+              request
+                .newBuilder()
+                .build()
+            )
+          }
+        })
+        .cache(Cache(requireActivity().cacheDir, (10 * 1024 * 1024).toLong()))
+        .build()
+
+      val retrofit: Api = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .client(okhttp)
+        .build()
+        .create()
+
+      binding.cache.setOnClickListener {
+        lifecycleScope.launch {
+          val r = retrofit.get()
+          Log.d("priorResponse", r.raw().priorResponse.toString())
+          Log.d("cacheResponse", r.raw().cacheResponse.toString())
+          Log.d("networkResponse", r.raw().networkResponse.toString())
+        }
+      }
+    }
+  }
+}
+
+interface Api {
+  @GET("users/satoshun")
+  suspend fun get(): Response<Unit>
+}
